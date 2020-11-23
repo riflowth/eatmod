7a1899438a79d03cec2f2980b63ccebc615039de
RiFlowTH <riflowth@gmail.com>
2020-11-24 00:07:42 +0700

    Fixed wrong path

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const apiController = require('../controller/api');
+const apiController = require('../controllers/api');
 const authMiddleware = require('../middlewares/auth');
 
 router.get(
