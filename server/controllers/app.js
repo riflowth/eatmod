e42ee7a7ecd5fa8dfa15b08e11a5eed70347b30b
RiFlowTH <riflowth@gmail.com>
2020-12-02 19:21:13 +0700

    Remove mocking data

--- server/controllers/app.js
+++ server/controllers/app.js
@@ -5,16 +5,7 @@ exports.getIndex = (req, res) => {
 
 exports.getShop = (req, res) => {
     const { id } = req.params;
-    res.render('shop', {
-        name: id,
-        type: 'ร้านก๋วยเตี๋ยว',
-        rating: 4.5,
-        allRatings: 120,
-        reviews: 50,
-        openTime: '08:00',
-        closeTime: '14:00',
-        location: 'ศูนย์อาหารพระจอมเกล้า (KFC)'
-    });
+    res.render('shop', {});
 };
 
 exports.getShops = (req, res) => {
