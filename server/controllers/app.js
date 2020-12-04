4e3d0eee8aa479ff0e6c583313b18ccf8fca78ca
eltfshr <puntf2@gmail.com>
2020-12-04 15:16:08 +0700

    Get shop from database

--- server/controllers/app.js
+++ server/controllers/app.js
@@ -1,8 +1,14 @@
-exports.getIndex = (req, res) => {
+const shop = require('../models/shop')
+
+
+
+exports.getIndex = async (req, res) => {
     // TODO
+    let shops = await shop.getShops();
     res.render(
         'index', {
-        
+           recommendMenus: [],
+           recommendShops: shops
         }
     );
 };
