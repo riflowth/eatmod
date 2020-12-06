fc765d7799c8be91324f5c145e8cab148217ab79
XiaoXuxxx <73125291+Aonrok@users.noreply.github.com>
2020-12-07 01:44:56 +0700

    Add foods table seeding/migration

---
+++ server/database/migrations/20201207011854_add-foods-table.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('foods', function(table) {
+        table.increments('id').primary();
+        table.string('name');
+        table.string('type');
+        table.integer('price');
+        table.string('image_url');
+        table.integer('shop_id');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('foods');
+};
