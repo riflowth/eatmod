2be6f679a3b12df7ed333060e9e617f39be4f955
Nathee Jaywaree <73115539+eltfshr@users.noreply.github.com>
2020-12-07 19:50:43 +0700

    Feat/shop (#13)
    * add write/getReview
    
    * Fix Error handling
    
    * add update/delete Reviews
    
    * add randomShops for index page & optimize getShop
    
    Co-authored-by: Krid Heprakhone <17198802+riflowth@users.noreply.github.com>

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,6 +1,16 @@
 const Shop = require('../models/shop');
 const Menu = require('../models/menu');
 
+exports.writeReview = (req, res) => {
+    let review = req.body.review;
+    let userId = 1;
+    let rating = req.body.rating;
+    let shopId = req.body.shopId;
+    Shop.writeReview(rating,userId,review,shopId);
+    
+    res.status(201).json({ success: true });
+};
+
 exports.addFoodData = (req, res) => {
     let id = req.body.id;
     let name = req.body.name;
--- server/controllers/app.js
+++ server/controllers/app.js
@@ -1,42 +1,56 @@
+const knex = require('../database/knex');
 const Shop = require('../models/shop')
 const Menu = require('../models/menu');
 
 exports.getIndex = async (req, res) => {
     let shops = await Shop.getShops();
+    let randomShops = [];
+  
+    for (let i = 0; i < 2; i++) {
+        do {
+            randomShops[i] = shops[Math.floor(Math.random() * shops.length)];
+        } while (new Set(randomShops).size != randomShops.length);
+    }
     let menus = await Menu.getAllMenuImages();
     let randomMenus = await Menu.findLastId();
 
     res.render(
         'index', {
            recommendMenus: [],
-           recommendShops: shops
+           recommendShops: randomShops
         }
     );
 };
 
 exports.getShop = async (req, res) => {
     const { id } = req.params;
-
-    let shop = await Shop.getShop(id);
-    let reviews = await Shop.getReviews(id);
-    let averageSum = 0;
-    let ratingSum = 0;
-
-    for (review of reviews) {
-        ratingSum = ratingSum + review.rating;
+  
+    try{
+        let shop = await Shop.getShop(id);
+        let reviews = await Shop.getReviews(id);
+        let averageSum = 0;
+        let ratingSum = 0;
+      
+        if (reviews.length != 0) {
+            for (review of reviews) {
+              ratingSum = ratingSum + review.rating;
+            }
+            averageSum = ratingSum/reviews.length;
+        }
+    
+        res.render('shop', {
+            name: shop.name,
+            type: shop.type,
+            location: shop.location,
+            rating: averageSum,
+            allRatings: ratingSum,
+            reviews: reviews.length,
+            openTime: shop.open,
+            closeTime: shop.close
+        });
+    } catch {
+        res.redirect('/shop');
     }
-    averageSum = ratingSum / reviews.length;
-
-    res.render('shop', {
-        name: shop.name,
-        type: shop.type,
-        location: shop.location,
-        rating: averageSum,
-        allRatings: ratingSum,
-        reviews: reviews.length,
-        openTime: shop.open,
-        closeTime: shop.close
-    });
 };
 
 exports.getShops = (req, res) => {
