099f48b41b90988b5b02b1594f4daa76b9667dd4
eltfshr <puntf2@gmail.com>
2020-12-07 15:16:11 +0700

    add randomShops for index page & optimize getShop

--- server/controllers/app.js
+++ server/controllers/app.js
@@ -2,36 +2,51 @@ const knex = require('../database/knex');
 const Shop = require('../models/shop')
 
 exports.getIndex = async (req, res) => {
-    // TODO
     let shops = await Shop.getShops();
+    let randomShops = [];
+    for(let i = 0; i < 2; i++){
+        do {
+            randomShops[i] = shops[Math.floor(Math.random() * shops.length)];
+        } while(new Set(randomShops).size != randomShops.length);
+    }
+    
     res.render(
         'index', {
            recommendMenus: [],
-           recommendShops: shops
+           recommendShops: randomShops
         }
     );
 };
 
 exports.getShop = async (req, res) => {
     const { id } = req.params;
-    let shop = await Shop.getShop(id);
-    let reviews = await Shop.getReviews(id);
-    let averageSum = 0;
-    let ratingSum = 0;
-    for(review of reviews){
-        ratingSum = ratingSum + review.rating;
+    try{
+        let shop = await Shop.getShop(id);
+        let reviews = await Shop.getReviews(id);
+        let averageSum = 0;
+        let ratingSum = 0;
+        if(reviews.length != 0){
+            for(review of reviews){
+               ratingSum = ratingSum + review.rating;
+            }
+            averageSum = ratingSum/reviews.length;
+        }
+    
+        res.render('shop', {
+            name: shop.name,
+            type: shop.type,
+            location: shop.location,
+            rating: averageSum,
+            allRatings: ratingSum,
+            reviews: reviews.length,
+            openTime: shop.open,
+            closeTime: shop.close
+        });
+    } catch{
+        res.redirect('/shop');
     }
-    averageSum = ratingSum/reviews.length;
-    res.render('shop', {
-        name: shop.name,
-        type: shop.type,
-        location: shop.location,
-        rating: averageSum,
-        allRatings: ratingSum,
-        reviews: reviews.length,
-        openTime: shop.open,
-        closeTime: shop.close
-    });
+    
+    
 };
 
 exports.getShops = (req, res) => {
