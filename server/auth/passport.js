ec888fe13cd355f97543224347e366887a5b9174
RiFlowTH <riflowth@gmail.com>
2020-12-08 22:02:17 +0700

    Add facebook passport middleware

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,19 +1,34 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const User = require('../models/user');
 
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECERT,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: 'http://localhost:8080/auth/google/callback'
     },
-    function (accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        done(null, profile);
+    function(accessToken, refreshToken, profile, done) {
+        User.getOrCreate(profile, (err, user) => {
+            return done(err, user);
+        });
+    }
+));
+
+passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: 'http://localhost:8080/auth/facebook/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.getOrCreate(profile, (err, user) => {
+            return done(err, user);
+        });
     }
 ));
 
 passport.serializeUser(function(user, done) {
-    done(null, user);
+    done(null, user.id);
 });
   
 passport.deserializeUser(function(id, done) {
