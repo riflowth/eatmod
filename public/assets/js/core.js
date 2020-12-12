6d60f844e1f697adf64257ce64d2f6f908c52df8
RiFlowTH <riflowth@gmail.com>
2020-12-13 01:15:36 +0700

    Complete review system without rating (test)

--- public/assets/js/core.js
+++ public/assets/js/core.js
@@ -14,7 +14,28 @@ $(function() {
             dataType: 'JSON',
             data: $(this).serialize() + '&rating=5'
         }).done((res) => {
-
+            Swal.fire({
+                title: 'คุณแน่ใจหรือไม่?',
+                text: 'การเขียนรีวิวผู้อื่นจะสามารถเข้ามาอ่านรีวิวที่คุณเขียนได้อย่างสาธารณะ คุณยืนยันที่จะบันทึกรีวิวหรือไม่',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#e94a26',
+                cancelButtonColor: '#dae0e5',
+                confirmButtonText: 'บันทึก',
+                cancelButtonText: 'ยกเลิก'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: 'บันทึกสำเร็จ!',
+                        text: 'ระบบได้บึนทักรีวิวของคุณเรียบร้อย',
+                        icon: 'success',
+                        showConfirmButton: false
+                    });
+                    setTimeout(() => {
+                        location.reload();
+                    }, 1500);
+                }
+            });
         }).fail((err) => {
 
         });
