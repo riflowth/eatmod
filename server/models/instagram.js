a69bcdedaf6a10f73b6f91f457b901df52557546
eltfshr <puntf2@gmail.com>
2020-12-13 20:30:58 +0700

    improve code

--- server/models/instagram.js
+++ server/models/instagram.js
@@ -11,12 +11,26 @@ for(let i = 0; i < 4; i++){
 }
 
 async function getInstagram() {
-    try {
-        let topHashTag = await client.getPhotosByHashtag({ hashtag: 'eatmod'});
-        if (Object.keys(new Object(topHashTag)).length != 0) {
-            topHashTag = topHashTag.hashtag.edge_hashtag_to_media.edges;
+    let topHashTag = await client.getPhotosByHashtag({ hashtag: 'eatmod'});
+    if (topHashTag != undefined) {
+        topHashTag = topHashTag.hashtag.edge_hashtag_to_media.edges;
+        topHashTag.forEach((post, i) => {
+            instagramFeeds[i] = {
+                imgUrl: post.node.thumbnail_src,
+                originUrl: `https://www.instagram.com/p/${post.node.shortcode}/`,
+                timestamp: post.node.taken_at_timestamp
+            }
+        })
+        instagramFeeds = instagramFeeds.filter((feed) => {
+            return ((Date.now() / 1000 - feed.timestamp) / 3600 / 24 / 30 ) <= 2;
+        });
+        instagramFeeds = instagramFeeds.slice(0,12);
+    } else {
+        let topHashTag = await client.getMediaFeedByHashtag({ hashtag: 'eatmod'});
+        if (topHashTag != undefined) {
+            topHashTag = topHashTag.edge_hashtag_to_media.edges;
             topHashTag.forEach((post, i) => {
-                instagramFeeds[i] = { 
+                instagramFeeds[i] = {
                     imgUrl: post.node.thumbnail_src,
                     originUrl: `https://www.instagram.com/p/${post.node.shortcode}/`,
                     timestamp: post.node.taken_at_timestamp
@@ -27,34 +41,10 @@ async function getInstagram() {
             });
             instagramFeeds = instagramFeeds.slice(0,12);
         } else {
-            throw 1;
-        }
-    } catch {
-        try {
-            let topHashTag = await client.getMediaFeedByHashtag({ hashtag: 'eatmod'});
-            if (Object.keys(new Object(topHashTag)).length != 0) {
-                topHashTag = topHashTag.edge_hashtag_to_media.edges;
-                topHashTag.forEach((post, i) => {
-                    instagramFeeds[i] = { 
-                        imgUrl: post.node.thumbnail_src,
-                        originUrl: `https://www.instagram.com/p/${post.node.shortcode}/`,
-                        timestamp: post.node.taken_at_timestamp
-                    }
-                })
-                instagramFeeds = instagramFeeds.filter((feed) => {
-                   return ((Date.now() / 1000 - feed.timestamp) / 3600 / 24 / 30 ) <= 2;
-                });
-                instagramFeeds = instagramFeeds.slice(0,12);
-            } else {
-                throw 1;
-            }
-        } catch {
             return 0;
         }
     }
 }
-
-
 
 exports.initialize = async () => {
     await getInstagram();
