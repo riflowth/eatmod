2b33924dda279f01551ebfd343c01b99493babf2
RiFlowTH <riflowth@gmail.com>
2021-06-10 23:10:16 +0700

    Beautify and remove depreacted warning

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 require('./config/config');
 
-const path = require('path');
-const color = require('chalk');
-const log = require('./utils/log');
+// Utility modules
+const path          = require('path');
+const color         = require('chalk');
+const log           = require('./utils/log');
 
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const passport = require('./auth/passport');
-const knex = require('./database/knex');
+// Main modules
+const express       = require('express');
+const session       = require('express-session');
+const passport      = require('./auth/passport');
+const knex          = require('./database/knex');
+const Instagram     = require('./models/instagram');
 
+// First check database connection
 knex.raw('select 0').catch(err => {
     log.error(log.prefix.DATABASE, `${err.sqlMessage || err.code}`);
     log.warn(log.prefix.WEB, `Server has closed...!`);
@@ -18,30 +21,28 @@ knex.raw('select 0').catch(err => {
     log.success(log.prefix.DATABASE, `The database connection established`);
 });
 
-const KnexSessionStore = require('connect-session-knex')(session);
-const Instagram = require('./models/instagram');
+Instagram.initialize();
 
-const app = express();
-const port = (process.env.PORT || 8080);
+const SessionStore  = require('connect-session-knex')(session);
+const app           = express();
+const port          = (process.env.PORT || 8080);
 
-const appRoute = require('./routes/app');
-const apiRoute = require('./routes/api');
-const authRoute = require('./routes/auth');
+// Directory
+const publicPath    = path.join(__dirname + '/../public');
+const viewPath      = path.join(publicPath + '/views');
 
-const publicPath = path.join(__dirname + '/../public');
-const viewPath = path.join(publicPath + '/views');
-
+// Initialize Express for sessioning and authentication
 app.set('view engine', 'ejs');
 app.set('views', viewPath);
 app.use(express.static(publicPath));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
         secret: 'eatmod-cpe34',
         saveUninitialized: false,
         resave: true,
-        store: new KnexSessionStore({
+        store: new SessionStore({
             knex: knex,
             tablename: 'sessions'
         })
@@ -50,19 +51,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// All routes
+const appRoute      = require('./routes/app');
+const apiRoute      = require('./routes/api');
+const authRoute     = require('./routes/auth');
+
 app.use('/', appRoute);
 app.use('/api', apiRoute);
 app.use('/auth', authRoute);
 
-Instagram.initialize();
-
 // Error Handler
 app.use((req, res) => {
-    if (req.url.startsWith('/assets/images')) {
-        res.redirect('https://lh3.googleusercontent.com/MgJS2D26eWVE9wOQ14vYQhVvot7NsgTsL9Fm-C8NwGC8Tn277Py-piniyyI5JkfyWLY4=s143');
-    } else {
-        res.redirect('/');
-    }
+    res.redirect('/');
 });
 
 app.listen(port, () => {
