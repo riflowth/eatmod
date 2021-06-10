77354aad012f90d4fc5cc2e2344642fb2e9f23a6
RiFlowTH <riflowth@gmail.com>
2021-06-10 20:52:22 +0700

    Merge into single function

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -3,28 +3,24 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('../models/user');
 
+const handleAuthentication = (accessToken, refreshToken, profile, done) => {
+    User.getOrCreate(profile, (err, user) => {
+        return done(err, user);
+    });
+}
+
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: process.env.GOOGLE_AUTH_CALLBACK
-    },
-    function(accessToken, refreshToken, profile, done) {
-        User.getOrCreate(profile, (err, user) => {
-            return done(err, user);
-        });
-    }
+    }, handleAuthentication
 ));
 
 passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
         callbackURL: process.env.FACEBOOK_AUTH_CALLBACK
-    },
-    function(accessToken, refreshToken, profile, done) {
-        User.getOrCreate(profile, (err, user) => {
-            return done(err, user);
-        });
-    }
+    }, handleAuthentication
 ));
 
 passport.serializeUser(function(user, done) {
